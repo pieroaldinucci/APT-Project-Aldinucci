706f15a5feec892c2416a61dd553438b9c630515
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-12-28 18:45:49 +0100

    Added AuthorView interface, to check dependancies

---
+++ bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/view/AuthorView.java
@@ -0,0 +1,13 @@
+package piero.aldinucci.apt.bookstore.view;
+
+import java.util.List;
+
+import piero.aldinucci.apt.bookstore.model.Author;
+
+public interface AuthorView {
+	
+	public void showAllAuthors(List<Author> authors);
+	public void authorAdded(Author author);
+	public void authorRemoved(Author author);
+	public void showError(String message, Author author);
+}
