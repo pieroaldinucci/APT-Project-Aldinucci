b3518b4497e8ecfef4c1760b1055b94ba5923b0b
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-12-29 15:27:13 +0100

    Start BookstoreController implementation

---
+++ bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/controller/BookstoreControllerImpl.java
@@ -0,0 +1,50 @@
+package piero.aldinucci.apt.bookstore.controller;
+
+import piero.aldinucci.apt.bookstore.model.Author;
+import piero.aldinucci.apt.bookstore.model.Book;
+
+public class BookstoreControllerImpl implements BookstoreController {
+
+	@Override
+	public void allAuthors() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void allBooks() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newAuthor(Author author) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteAuthor(Author author) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void composeBook() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bookstore-mvc/src/test/java/piero/aldinucci/apt/bookstore/controller/BookstoreControllerImplTest.java
@@ -0,0 +1,14 @@
+package piero.aldinucci.apt.bookstore.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class BookstoreControllerImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
