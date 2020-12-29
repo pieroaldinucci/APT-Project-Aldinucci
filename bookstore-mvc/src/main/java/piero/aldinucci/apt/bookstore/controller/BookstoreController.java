6732368aa4ac6811293c51a55fc2a66ac1f46a22
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-12-29 15:16:23 +0100

    Added controller Interface

---
+++ bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/controller/BookstoreController.java
@@ -0,0 +1,14 @@
+package piero.aldinucci.apt.bookstore.controller;
+
+import piero.aldinucci.apt.bookstore.model.Author;
+import piero.aldinucci.apt.bookstore.model.Book;
+
+public interface BookstoreController {
+	public void allAuthors();
+	public void allBooks();
+	public void newBook(Book book);
+	public void newAuthor(Author author);
+	public void deleteBook(Book book);
+	public void deleteAuthor(Author author);
+	public void composeBook();
+}
