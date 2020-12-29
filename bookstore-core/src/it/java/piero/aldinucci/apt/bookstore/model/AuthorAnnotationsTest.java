355e5d2130864bf3b0fc79d9f6d272e59fca4b05
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-12-29 11:40:14 +0100

    Added integration test for Author annotations.

---
+++ bookstore-core/src/it/java/piero/aldinucci/apt/bookstore/model/AuthorAnnotationsTest.java
@@ -0,0 +1,56 @@
+package piero.aldinucci.apt.bookstore.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AuthorAnnotationsTest {
+	
+	EntityManagerFactory emFactory;
+	
+	@Before
+	public void setUp() {
+		emFactory = Persistence.createEntityManagerFactory("apt.project.bookstore");
+		EntityManager entityManager = emFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		entityManager.createQuery("from Author", Author.class).getResultStream()
+			.forEach(a -> entityManager.remove(a));
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+	
+	@After
+	public void tearDown() {
+		emFactory.close();
+	}
+
+	@Test
+	public void test_generatedID_and_Eager_Initialization() {
+		Author author = new Author(null, "an Author", new HashSet<>());
+		EntityManager entityManager = emFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(author);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+		assertThat(author.getId()).isNotNull();
+		
+		entityManager = emFactory.createEntityManager();
+		Author retrievedAuthor = entityManager.find(Author.class, author.getId());
+		entityManager.close();
+		
+		assertThat(retrievedAuthor).usingRecursiveComparison().isEqualTo(author);
+	}
+
+}
