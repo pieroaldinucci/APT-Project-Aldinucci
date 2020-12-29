401b896c5ae4bd857b8a50ce22f5401cc0e98252
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-12-29 15:21:28 +0100

    Added service layer interface

---
+++ bookstore-core/src/main/java/piero/aldinucci/apt/bookstore/service/BookstoreManager.java
@@ -0,0 +1,17 @@
+package piero.aldinucci.apt.bookstore.service;
+
+import java.util.List;
+
+import piero.aldinucci.apt.bookstore.model.Author;
+import piero.aldinucci.apt.bookstore.model.Book;
+
+public interface BookstoreManager {
+	public Book newBook(Book book);
+	public Author newAuthor(Author author);
+	public void delete(Author author);
+	public void delete(Book book);
+	public List<Author> getAllAuthors();
+	public List<Book> getAllBooks();
+	public void update(Author author);
+	public void update(Book book);
+}
