aa6ebe6432c5afc86b5143b3c056d09029d13538
Piero Aldinucci <piero.aldinucci@gmail.com>
2021-01-01 16:03:36 +0100

    Added BookstoreManagerImpl class

---
+++ bookstore-core/src/main/java/piero/aldinucci/apt/bookstore/service/BookstoreManagerImpl.java
@@ -0,0 +1,58 @@
+package piero.aldinucci.apt.bookstore.service;
+
+import java.util.List;
+
+import piero.aldinucci.apt.bookstore.model.Author;
+import piero.aldinucci.apt.bookstore.model.Book;
+
+public class BookstoreManagerImpl implements BookstoreManager {
+
+	@Override
+	public Book newBook(Book book) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Author newAuthor(Author author) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Author author) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Author> getAllAuthors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> getAllBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Author author) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bookstore-core/src/test/java/piero/aldinucci/apt/bookstore/service/BookstoreManagerImplTest.java
@@ -0,0 +1,14 @@
+package piero.aldinucci.apt.bookstore.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class BookstoreManagerImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
