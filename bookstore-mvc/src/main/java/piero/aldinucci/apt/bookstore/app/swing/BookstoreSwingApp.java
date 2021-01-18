e2022fd484e6e510d5e3fb78970256821300c516
Piero Aldinucci <piero.aldinucci@gmail.com>
2021-01-18 16:28:01 +0100

    Added picocli to handle application launch.

--- bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/app/swing/BookstoreSwingApp.java
+++ bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/app/swing/BookstoreSwingApp.java
@@ -1,6 +1,8 @@
 package piero.aldinucci.apt.bookstore.app.swing;
 
 import java.awt.EventQueue;
+import java.util.HashMap;
+import java.util.concurrent.Callable;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -8,6 +10,9 @@ import javax.persistence.Persistence;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import piero.aldinucci.apt.bookstore.app.guice.modules.BookstoreControllerSwingModule;
 import piero.aldinucci.apt.bookstore.app.guice.modules.BookstoreManagerJPAModule;
 import piero.aldinucci.apt.bookstore.controller.BookstoreControllerImpl;
@@ -15,9 +20,41 @@ import piero.aldinucci.apt.bookstore.view.swing.AuthorSwingView;
 import piero.aldinucci.apt.bookstore.view.swing.BookSwingView;
 import piero.aldinucci.apt.bookstore.view.swing.BookstoreSwingFrame;
 
-public class BookstoreSwingApp{
+@Command(mixinStandardHelpOptions = true)
+public class BookstoreSwingApp implements Callable<Void>{
+	
+	@Option(names = {"--postgres-host"}, description = {"Postgresql host address"})
+	private String host = "localhost";
+	
+	@Option(names = {"--db-name"}, description = {"Database name"})
+	private String databaseName = "projectAPTTestDb";
+	
+	@Option(names = {"--postgres-port"}, description = {"Postgresql host port"})
+	private int port = 5432;
+	
+	@Option(names = {"--user"}, description = {"Postgresql username"})
+	private String userName = "testUser";
+	
+	@Option(names = {"--password"}, description = {"Postgresql password"})
+	private String password = "secret";
+	
 	
 	public static void main(String[] args){
+		new CommandLine(new BookstoreSwingApp()).execute(args);
+	}
+
+	private EntityManagerFactory getEntityManagerFactory() {
+		String propertyJdbcUrl = "jdbc:postgresql://"+host+":"+port+"/"+databaseName;
+		
+		HashMap<String, String> propertiesJPA = new HashMap<>();
+		propertiesJPA.put("javax.persistence.jdbc.user", userName);
+		propertiesJPA.put("javax.persistence.jdbc.password", password);
+		propertiesJPA.put("javax.persistence.jdbc.url", propertyJdbcUrl);
+		return Persistence.createEntityManagerFactory("apt.project.bookstore",propertiesJPA);
+	}
+
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {			
 			try {
 				Injector injector = Guice.createInjector(new BookstoreControllerSwingModule(
@@ -34,9 +71,6 @@ public class BookstoreSwingApp{
 				e.printStackTrace();
 			}
 		});
-	}
-
-	private static EntityManagerFactory getEntityManagerFactory() {
-		return Persistence.createEntityManagerFactory("apt.project.bookstore");
+		return null;
 	}
 }
