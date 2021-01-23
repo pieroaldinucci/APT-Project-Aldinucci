cd72b208949a2ecce56e0e7c962879742b55dbef
Piero Aldinucci <piero.aldinucci@gmail.com>
2021-01-23 23:20:05 +0100

    Comment indentation

--- bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/view/ComposeBookView.java
+++ bookstore-mvc/src/main/java/piero/aldinucci/apt/bookstore/view/ComposeBookView.java
@@ -6,6 +6,7 @@ import piero.aldinucci.apt.bookstore.model.Author;
 
 /**
  * This view is used to create new Book objects, using the available authors.
+ * 
  * @author Piero Aldinucci
  *
  */
@@ -13,6 +14,7 @@ public interface ComposeBookView {
 
 	/**
 	 * Initializa and show the view
+	 * 
 	 * @param authors List of available authors
 	 */
 	public void composeNewBook(List<Author> authors);
